775707d9240c06434b16a93d35494beb77fae854
HBSPS <wiyeong79@naver.com>
2024-06-06 23:19:23 +0900

    fix: tree 각 노드 간격 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Tree from 'react-d3-tree';
 function App() {
   return (
     <div style={{ width: '98vw', height: '98vh' }}>
-      <Tree data={data} pathFunc='step' orientation='vertical' translate={{ x: 500, y: 250 }} />
+      <Tree data={data} pathFunc='step' orientation='vertical' translate={{ x: 500, y: 250 }} separation={{ nonSiblings: 3, siblings: 3 }} />
     </div>
   )
 }
