f80024b7a4cdbb44cd9b27e1aa323e61f7432f42
HBSPS <wiyeong79@naver.com>
2024-06-07 15:16:52 +0900

    refactor: resolveImportPaths 로직 개선

--- plugin/resolveImportPaths.ts
+++ plugin/resolveImportPaths.ts
@@ -2,17 +2,18 @@ import { join } from 'path';
 import { configPath } from './getConfigFile';
 
 function pathMapping(aliasPathWithAlias: string, baseUrl: string, paths: configPath) {
+  let resolvedPath = aliasPathWithAlias;
+
   for (const [aliasName, actualPath] of Object.entries(paths)) {
     const aliasPrefix = aliasName.replace('*', '');
 
     if (aliasPathWithAlias.startsWith(aliasPrefix)) {
-      const resolvedPath = aliasPathWithAlias.replace(aliasPrefix, actualPath[0].replace('*', ''));
-
-      return join(baseUrl, resolvedPath).replace(/\\/g, '/');
+      resolvedPath = aliasPathWithAlias.replace(aliasPrefix, actualPath[0].replace('*', ''));
+      break;
     }
   }
 
-  return aliasPathWithAlias;
+  return join(baseUrl, resolvedPath).replace(/\\/g, '/');
 }
 
 export function resolveImportPaths(imports: string[], baseUrl: string, paths: configPath) {
