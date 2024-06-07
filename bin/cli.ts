19177931a5a8988c2a07a9f3b60993cd8c56d90d
HBSPS <wiyeong79@naver.com>
2024-06-07 15:44:05 +0900

    feat: package.json main 추가

--- bin/cli.ts
+++ bin/cli.ts
@@ -9,17 +9,19 @@ import { getConfigFile } from '../plugin/getConfigFile';
 import { FileTree } from '../plugin/FileTree';
 import { generateTemplate } from '../plugin/generateTemplate';
 
-const { root, targetDir } = cliConfig(process.argv.slice(2));
+export default async function cli() {
+  const { root, targetDir } = cliConfig(process.argv.slice(2));
 
-const { compilerOptions } = getConfigFile();
-const { baseUrl, paths } = compilerOptions;
+  const { compilerOptions } = getConfigFile();
+  const { baseUrl, paths } = compilerOptions;
 
-const fileTree = new FileTree(root, targetDir, baseUrl, paths);
-generateTemplate(fileTree.tree);
+  const fileTree = new FileTree(root, targetDir, baseUrl, paths);
+  generateTemplate(fileTree.tree);
 
-const resultFilePath = resolve(process.argv[1], '../../index.html');
-await open(resultFilePath, { wait: true });
+  const resultFilePath = resolve(process.argv[1], '../../index.html');
+  await open(resultFilePath, { wait: true });
 
-console.log('Tree has been created. Check your browser.');
+  console.log('Tree has been created. Check your browser.');
 
-await unlink(resultFilePath);
+  await unlink(resultFilePath);
+}
