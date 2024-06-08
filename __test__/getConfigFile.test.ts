e16071ab2fe54046ae7fa629f8843799c885df1d
HBSPS <wiyeong79@naver.com>
2024-06-08 15:14:56 +0900

    test: getConfigFile 테스트 코드 추가

---
+++ __test__/getConfigFile.test.ts
@@ -0,0 +1,129 @@
+import fs from 'fs';
+import { getConfigFile } from '../plugin/getConfigFile';
+
+describe('getConfigFile', () => {
+  test('config with baseUrl and paths', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => '{ compilerOptions: { baseUrl: "./src", paths: { "@/page/*": ["page/*"] } } }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: './src',
+        paths: {
+          '@/page/*': ['page/*'],
+        },
+      },
+    });
+  });
+
+  test('config with baseUrl', () => {
+    jest.spyOn(fs, 'readFileSync').mockImplementationOnce(() => '{ compilerOptions: { baseUrl: "./src" } }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: './src',
+        paths: {},
+      },
+    });
+  });
+
+  test('config without any alias', () => {
+    jest.spyOn(fs, 'readFileSync').mockImplementationOnce(() => '{ compilerOptions: {} }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: '',
+        paths: {},
+      },
+    });
+  });
+
+  test('without config file', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      })
+      .mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: '',
+        paths: {},
+      },
+    });
+  });
+});
+
+describe('getConfigFile (JS)', () => {
+  test('config with baseUrl and paths', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      })
+      .mockImplementationOnce(() => '{ compilerOptions: { baseUrl: "./src", paths: { "@/page/*": ["page/*"] } } }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: './src',
+        paths: {
+          '@/page/*': ['page/*'],
+        },
+      },
+    });
+  });
+
+  test('config with baseUrl', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      })
+      .mockImplementationOnce(() => '{ compilerOptions: { baseUrl: "./src" } }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: './src',
+        paths: {},
+      },
+    });
+  });
+
+  test('config without any alias', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      })
+      .mockImplementationOnce(() => '{ compilerOptions: {} }');
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: '',
+        paths: {},
+      },
+    });
+  });
+
+  test('without config file', () => {
+    jest
+      .spyOn(fs, 'readFileSync')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      })
+      .mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+    expect(getConfigFile()).toStrictEqual({
+      compilerOptions: {
+        baseUrl: '',
+        paths: {},
+      },
+    });
+  });
+});
