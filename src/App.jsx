a4747bbed3f7a456e531a411cc15892722a45307
HBSPS <wiyeong79@naver.com>
2024-07-01 18:10:10 +0900

    refactor: separate CustomNode as a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Tree from 'react-d3-tree';
 import TextSizeController from './TextSizeController.jsx';
 import Controller from './Controller.jsx';
+import CustomNode from './CustomNode.jsx';
 
 const SEPERATION_INTERVAL = 0.5;
 const DEPTH_INTERVAL = 50;
@@ -33,22 +34,6 @@ function App() {
     setSeperation((prev) => (prev - SEPERATION_INTERVAL > 0 ? prev - SEPERATION_INTERVAL : prev));
   };
 
-  const customNodeRender = ({ nodeDatum, toggleNode }) => (
-    <>
-      <circle r='15' onClick={toggleNode}></circle>
-      <g class='rd3t-label'>
-        <text class='rd3t-label__title' text-anchor='center' x='20' y='20'>
-          {nodeDatum.name}
-        </text>
-        <text class='rd3t-label__attributes'>
-          <tspan x='20' y='20' dy='1.2em'>
-            dir: {nodeDatum.attributes.dir}
-          </tspan>
-        </text>
-      </g>
-    </>
-  );
-
   return (
     <div style={{ width: '98vw', height: '95vh' }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 10 }}>
@@ -73,7 +58,7 @@ function App() {
         leafNodeClassName='node__leaf'
         initialDepth={collapse ? 1 : undefined}
         depthFactor={depthFactor}
-        renderCustomNodeElement={(rd3Props) => customNodeRender({ ...rd3Props })}
+        renderCustomNodeElement={(rd3Props) => CustomNode({ ...rd3Props })}
       />
     </div>
   );
